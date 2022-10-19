8c1b5496e7e02d14e4ab4b9e18a6f68a1884dfaa
sujin lim <swuujin990827@naver.com>
2022-10-20 06:35:15 +0900

    @ManyToMany, 계층형 메서드 설명

---
+++ src/main/java/com/study/board/domain/Category.java
@@ -0,0 +1,34 @@
+package com.study.board.domain;
+
+import com.study.board.domain.item.Item;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns=@JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    //연관관계 메서드 계층형이기때문
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
